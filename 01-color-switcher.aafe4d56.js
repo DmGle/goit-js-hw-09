b8081dfd4a060587a9eb81c128f80fb0445dc51e
DmGle <dimon.gleda1@gmail.com>
2023-11-17 19:40:09 +0200

    err

--- 01-color-switcher.aafe4d56.js
+++
@@ -1,3 +0,0 @@
-!function(){var t;function e(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}document.querySelector("[data-start]").addEventListener("click",function(){document.querySelector("[data-start]").disabled=!0,document.querySelector("[data-stop]").disabled=!1,e(),t=setInterval(e,1e3)}),document.querySelector("[data-stop]").addEventListener("click",function(){document.querySelector("[data-start]").disabled=!1,document.querySelector("[data-stop]").disabled=!0,clearInterval(t)})}();//# sourceMappingURL=01-color-switcher.aafe4d56.js.map
-
-//# sourceMappingURL=01-color-switcher.aafe4d56.js.map
